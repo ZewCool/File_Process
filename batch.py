889502cd353dcb1fefe5f9d81b199204a2c1c84a
ZewCool <51275730+ZewCool@users.noreply.github.com>
2019-11-01 10:44:37 +0000

    Update batch.py

--- batch.py
+++ batch.py
@@ -1,79 +1,71 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 30 11:21:30 2019
-
 @author: guz4
 """
 
 import os
 
-### Part One: count the number of samples by 'sample_num()'. 
-###           Rename every sample folders with an end of '00x' or '0xx'. 
-###           Modify the random number in every 'RandomFibre' file by looking for the '00x' in the name of folder. 
-
 def sample_num(path):                       
     ''' count the number of sample folders under 'path'. '''   
-    names = os.listdir(path)                               # get the names of all files and folders to a list
-    return FileProp(names, len(names))                                   # return the number of samples
-    
-def markFolders(numSample, markOrder=3, startNum=0):
-    ''' the mark value is the order of folder with certain format'''
-    mark = str(startNum + numSample).zfill(markOrder)
-    return mark
-    
-def rename_folders(oldName, mark, newName):
+    names = os.listdir(path)                 # get the names of all objects under the path and save to a list
+    return FileProp(names, len(names))       # return the number of subfolders
+
+def rename_folders(path, oldName, newName, mark):
     ''' rename the names of folders and files under 'path' by 'newName'. '''
     os.rename(os.path.join(path,oldName),
-              os.path.join(path,newName + mark))  
-    return NewName(newName)
-                  
+              os.path.join(path,newName + str(mark)))  
+    return 
 
-class FileProp:
-    def __init__(self, names, sampleNum):
+class FileName(object):
+    def __init__(self, names):
         self.FoNames = names
-        self.SaNum = sampleNum
         
-class NewName:
-    def __init__(self, newName):
-        self.NewName = newName
+class FileProp(FileName):
+    ''' the properties of the folders under 'path'. '''
+    def __init__(self, names, sampleNum):
+        FileName.__init__(self, names)
+        self.SaNum = sampleNum 
         
+    def __call__(self):
+        return self.SaNum       
+  
 class ModifyFiles(object):
-    def __init__(self, modLine, newContent):
-        self.ModLine = modLine
-        self.NewCont = newContent
-    
-    def __call__(self, filePath):
-        self.modify_file_line(filePath)
-        return
+    ''' 
+    modify the line 'modeLine' in the file 'filePath' by 'newContent'
+    the modLine should be an int. 
+    '''
+    def __init__(self, filePath):
+        self.filePath = filePath
         
-    def modify_file_line(self, filePath):
-        ''' modify the line 'modeLine' in the file 'filePath' by 'newContent'
-            the modLine should be an int. '''
-        with open(filePath) as f:
+    def modify_file_line(self, modLine, newContent):
+        with open(self.filePath) as f:
             lines = []
             for line in f:
                 lines.append(line)       
-            lines[self.ModLine] = self.NewCont
+            lines[modLine] = newContent 
             f.close() 
         # replace the old content  by newLine in 'lines'
-        with open(filePath, 'w') as fw:   
+        with open(self.filePath, 'w') as fw:   
             for newLine in lines:
                 fw.write(newLine)                             # write the new content into 'RandomFibre.fis'
-            fw.close()
+            fw.close()          
         return
+    
+def main_func(startNum=0, digitNum=3):  
+    '''
+    example main function which rename multiple folders and 
+    modify the file 'randomFibre' under each folder
+    '''
+    for oldName in sample_num(path).FoNames:
+        startNum += 1
+        mark = str(startNum).zfill(digitNum)
+        rename_folders(path, oldName, newName, mark)
         
-def mainFunc_part01():
-    FP = sample_num(path)                                 # assign the values in the Class 'FileProp' to 'FP'
-    mfp = ModifyFiles(1, 'SET random 10%s\n')
-    for numSample in range(FP.SaNum):
-        mark = markFolders(numSample)
-        rename_folders(FP.FoNames[numSample], mark, newName)   
+        filePath = path + '\\%s%s\\RandomFibre.fis' % (newName, mark)
+        modify_files = ModifyFiles(filePath)  
+        modify_files.modify_file_line(1, 'SET random 10%s\n' % mark)
         
-        filePath = path + '\\%s%s\\%s' % (newName, mark, fileName)
-        mfp(filePath)
-
-if __name__ == '__main__':
+if __name__ == '__main__':     
     path = 'C:\\PhD_ValveSpringDynamics\\Others\\DingML\\SBoard_OneFibSamples'
     newName = 'Sample'
-    fileName = 'RandomFibre.fis'
-    mainFunc_part01()
+    main_func()
