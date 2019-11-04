0438234eae5ad65befd2bcd5e314de01ee9253dd
ZewCool <51275730+ZewCool@users.noreply.github.com>
2019-11-04 16:59:30 +0000

    Update FileProcessMain.py

--- FileProcessMain.py
+++ FileProcessMain.py
@@ -4,6 +4,7 @@
 """
 
 import FileProcessFunctions as FPF
+import numpy as np
 
 def fibre_xyr(filePath, readStart, readEnd):    ## output fiber centre coordinates of file number under 'folderNum'
     fibInfo = []
@@ -14,6 +15,27 @@ def fibre_xyr(filePath, readStart, readEnd):    ## output fiber centre coordinat
     for fib in fibInfo:
         fibx.append(fib[1]), fiby.append(fib[2]), fibr.append(fib[3])
     return fibx, fiby, fibr
+
+def matrix_result(filePath, readStart, readEnd, blackBall=False):
+    matInfo = FPF.Files(filePath).read_file(readStart, readEnd) 
+    matInfoStr = []
+    for eachBall in matInfo:
+        if blackBall == False:
+            if eachBall[-2] == '0':
+                matInfoStr.append(eachBall.split())
+        if blackBall == True:
+            matInfoStr.append(eachBall.split())
+    return matInfoStr
+
+def crackId_result(filePath, readStart, readEnd, matInfoStr):
+    crackInfo = FPF.Files(filePath).read_file(readStart, readEnd)
+    matCid = []
+    for eachBall in matInfoStr:
+        if eachBall[0] + '\n' in set(crackInfo):
+            matCid.append('1')
+        else:
+            matCid.append('0')  
+    return matCid
 
 def file_path(num, mark):
     fileName = fileNames[num]
@@ -54,20 +76,54 @@ def main_func(stepNum=3, startNum=0, digitNum=3):
                           '  loop _kk (1, %s)\n' % len(fibR)]
             for line, cont in zip(linNums, newContent):
                 fileHex.modify_file_line(line, cont)
+    
+    if stepNum == 3:
+        marx, mary, crackId, fibCx, fibCy = [], [], [], [], []
+        for oldName in FPF.sample_num(path).FoNames:
+            startNum += 1
+            mark = str(startNum).zfill(digitNum)
+            
+            filePathOut = file_path(3, mark)
+            matInfoStr = matrix_result(filePathOut, 'matrix_and_fibre start\n', 'matrix_and_fibre end\n', blackBall=False)
+            matCidStr = crackId_result(filePathOut, 'cracksid start\n', 'cracksid end\n', matInfoStr)
+            filePathFib = file_path(1, mark)
+            fibXStr, fibYStr, fibRStr = fibre_xyr(filePathFib, 'FibreCentreStart\n', 'FibreCentreEnd\n')
+            
+            # change the type from 'string' to 'float'
+            matInfoFloat = FPF.map_str_to_float(matInfoStr)
+            matCidFloat = FPF.map_str_to_float(matCidStr)
+            fibXFloat, fibYFloat = list(map(float, fibXStr)), list(map(float, fibYStr))
+            
+            # make the data in each board as an array
+            matInfoArr, matCidArr = np.array(matInfoFloat), np.array(matCidFloat)   
+            fibXArr, fibYArr = np.array(fibXFloat), np.array(fibYFloat)
+            
+            # achieve datas on each board
+            marXEachB, marYEachB, crackIdEachB = matInfoArr[:, 1], matInfoArr[:, 2], matCidArr[:]
+            fibCxEachB, fibCyEachB = fibXArr[:], fibYArr[:]
+            
+            # list the datas of all boards
+            marx.append(marXEachB)
+            mary.append(marYEachB)
+            crackId.append(crackIdEachB)
+            fibCx.append(fibCxEachB)
+            fibCy.append(fibCyEachB)
+            
+        return marx, mary, crackId, fibCx, fibCy
+                   
         
 if __name__ == '__main__':   
     # parameters defined outside functions
     path = 'C:\\PhD_ValveSpringDynamics\\Others\\DingML\\SBoardSamples'
     newName = 'Sample'
-    fileNames = ['RandomFibre.fis','FibreCentre.log', 'Hex1.fis']
+    fileNames = ['RandomFibre.fis',
+                 'FibreCentre.log', 
+                 'Hex1.fis', 
+                 'output1.log']
     
     # main function 
     # stepNum == 1 when we want to change the names of sub-folders 
     # stepNum == 2 when we want to change the content in several files
-    main_func(stepNum=2)
-
-               
-               
-
-
+    # stepNum == 3 can achieve the matrix and fibre data
     
+    marx, mary, crackId, fibCx, fibCy = main_func(stepNum=3)
